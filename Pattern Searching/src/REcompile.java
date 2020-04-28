2d866e576b70b6b066d825acd204c99b4e2ac881
cornpoid <cornpoid@users.noreply.github.com>
2020-04-28 14:59:17 +1200

    Implemented Tony's Code

--- Pattern Searching/src/REcompile.java
+++ Pattern Searching/src/REcompile.java
@@ -1,4 +1,5 @@
 import java.util.*;
+import java.io.*;
 
 public class REcompile {
 	//Initialize pattern array and FSM 
@@ -7,21 +8,46 @@ public class REcompile {
 	public static List<Integer> next1 = new ArrayList<Integer>();
 	public static List<Integer> next2 = new ArrayList<Integer>();
 	public static int state;
+	public static int j;
 	
 	/*Main Method*/
-	public static void main (String argsp[]) {
+	public static void main (String args[]) throws IOException {
 		//Take in pattern P (IO)
-		String line = argsp[0];
+		String line = args[0];
 		for(int i = 0; i < line.length(); i++) {
 			pattern.add(line.charAt(i));
-		}	
+		}
 		/*Parsing and Compiling*/
 		//initialize state
 		state = 0;
+		j = 0;
 		//Call expression
+		int initial = expression();
 		//If p[j] != '\0' error()
+		if(pattern.get(j) != null) {
+			error();
+		}
 		//set state
 		setState(state, ' ', 0, 0);
+		//write arrays to file
+		FileWriter writer = new FileWriter("ch");
+		for(char c: ch) {
+			writer.write(c + System.lineSeparator());
+		}
+		writer.close();
+		writer = new FileWriter("next1");
+		for (int i: next1) {
+			writer.write(i + System.lineSeparator());
+		}
+		writer.close();
+		writer = new FileWriter("next2");
+		for (int i: next2) {
+			writer.write(i + System.lineSeparator());
+		}
+		writer.close();
+		writer = new FileWriter ("start");
+		writer.write(initial);
+		writer.close();
 		
 	}
 	
@@ -39,7 +65,7 @@ public class REcompile {
 	}
 	
 	/*isVocab method - checks char to be not an operator*/
-	private boolean isVocab(char c) {
+	private static boolean isVocab(char c) {
 		if (c == '\\'||c == '?'||c != '('||c == ')'||c == '|'||c == '.'||c == '*'){
 			  return false;
 		}
@@ -47,22 +73,86 @@ public class REcompile {
 	}
 	
 	/*expression method*/
-	private int expression() {
+	private static int expression() {
 		int r;
+		//call term
 		r = term();
+		//call expression recursively if vocab or open parantheses
+		if(isVocab(pattern.get(j))||pattern.get(j)=='('){
+			expression();
+		}
+		//return initial state
 		return(r);
 	}
 	
 	/*term method*/
-	private int term() {
+	private static int term() {
+		//initialize variables
 		int r, t1, t2, f;
+		f = state - 1;
+		r = t1 = factor();
+		//if *
+		if(pattern.get(j)=='*') {
+			setState(state,' ',state+1,t1);
+			j++;
+			r=state;
+			state++;
+		}
+		//if |
+		if(pattern.get(j)=='|') {
+			if(next1.get(f)==next2.get(f)) {
+				next2.add(f, state);
+			}
+			next1.add(f, state);
+			f=state-1;
+			j++;
+			r=state;
+			state++;
+			t2=term();
+			setState(r, ' ', t1, t2);
+			if(next1.get(f)==next2.get(f)) {
+				next2.add(f,state);
+			}
+			next1.add(f, state);
+		}
+		//if ?
+		if(pattern.get(j)=='?') {
+			
+		}
+		//if \
+		if(pattern.get(j)=='\\') {
+			
+		}
+		//if .
+		if(pattern.get(j)=='.') {
+			
+		}
+		//return initial state
 		return(r);
 	}
 	
 	/*factor method*/
-	private int factor() {
-		int r;
+	private static int factor() {
+		int r = 0;
+		if(isVocab(pattern.get(j))) {
+			setState(state,pattern.get(j),state+1, state+1);
+			j++;
+			r=state;
+			state++;
+		}
+		else if(pattern.get(j)=='(') {
+			j++;
+			r=expression();
+			if(pattern.get(j)==')'){
+				j++;
+			}
+			else {
+				error();
+			}
+		}
+		else {
+			error();
+		}
 		return(r);
-		
 	}
 }
