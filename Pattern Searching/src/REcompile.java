891baae2eb68d10c5628426442c69cfcb314de38
cornpoid <cornpoid@users.noreply.github.com>
2020-05-04 17:22:38 +1200

    Possibly working version
    Needs more testing

--- Pattern Searching/src/REcompile.java
+++ Pattern Searching/src/REcompile.java
@@ -4,7 +4,7 @@ import java.io.*;
 public class REcompile {
 	//Initialize pattern array and FSM 
 	public static List<Character> pattern = new ArrayList<Character>();
-	public static List<Character> ch = new ArrayList<Character>();
+	public static List<String> ch = new ArrayList<String>();
 	public static List<Integer> next1 = new ArrayList<Integer>();
 	public static List<Integer> next2 = new ArrayList<Integer>();
 	public static int state;
@@ -24,14 +24,14 @@ public class REcompile {
 		//Call expression
 		int initial = expression();
 		//If p[j] != '\0' error()
-		if(pattern.get(j) != null) {
+		if(isNotEmpty()) {
 			error();
 		}
 		//set state
-		setState(state, ' ', 0, 0);
+		setState(state, " ", 0, 0);
 		//write arrays to file
 		FileWriter writer = new FileWriter("ch");
-		for(char c: ch) {
+		for(String c: ch) {
 			writer.write(c + System.lineSeparator());
 		}
 		writer.close();
@@ -46,16 +46,49 @@ public class REcompile {
 		}
 		writer.close();
 		writer = new FileWriter ("start");
-		writer.write(initial);
+		writer.write(Integer.toString(initial));
 		writer.close();
 		
 	}
 	
 	/*set state method*/
-	private static void setState(int s, char c, int n1, int n2) {
-		ch.add(s, c);
-		next1.add(s, n1);
-		next2.add(s, n2);
+	private static void setState(int s, String c, int n1, int n2) {
+		//fills empty array with empty characters to index to put in
+		if(s >= ch.size()) {
+			for(int i = (ch.size()); i < s; i++) {
+				if(i >= 0) {
+					ch.add(i, " ");
+				}
+			}
+			ch.add(s, c);
+
+		}
+		else {
+			ch.set(s, c);
+		}
+		if(s >= next1.size()) {
+			for(int i = (next1.size()); i < s; i++) {
+				if(i >= 0) {
+					next1.add(i, null);
+				}
+			}
+			next1.add(s, n1);
+		}
+		else {
+			next1.set(s, n1);
+		}
+		if(s >= next2.size()) {
+			for(int i = (next2.size()); i < s; i++) {
+				if(i >= 0) {
+					next2.add(i, null);
+				}
+			}
+			next2.add(s, n2);
+		}
+		else {
+
+			next2.set(s, n2);
+		}
 	}
 	
 	/*error method*/
@@ -64,12 +97,16 @@ public class REcompile {
 		System.exit(0);
 	}
 	
-	/*isVocab method - checks char to be not an operator*/
+	/*isVocab method - checks char to be not an operator and valid letter or digit*/
 	private static boolean isVocab(char c) {
-		if (c == '\\'||c == '?'||c != '('||c == ')'||c == '|'||c == '.'||c == '*'){
-			  return false;
-		}
-		return true;
+		String s = Character.toString(c);
+		String spec="?|*\\()"; 
+		return !spec.contains(s);
+	}
+	
+	/*check if empty pattern*/
+	private static boolean isNotEmpty() {
+		return j < pattern.size();
 	}
 	
 	/*expression method*/
@@ -78,8 +115,10 @@ public class REcompile {
 		//call term
 		r = term();
 		//call expression recursively if vocab or open parantheses
-		if(isVocab(pattern.get(j))||pattern.get(j)=='('){
-			expression();
+		if(isNotEmpty()) {
+			if(isVocab(pattern.get(j))||pattern.get(j)=='('||pattern.get(j)=='\\'){
+				expression();
+			}
 		}
 		//return initial state
 		return(r);
@@ -91,41 +130,42 @@ public class REcompile {
 		int r, t1, t2, f;
 		f = state - 1;
 		r = t1 = factor();
-		//if *
-		if(pattern.get(j)=='*') {
-			setState(state,' ',state+1,t1);
+		//if * sets states appropriately
+		if(isNotEmpty() && pattern.get(j)=='*') {
+			setState(state," ",state+1,t1);
 			j++;
 			r=state;
 			state++;
 		}
-		//if |
-		if(pattern.get(j)=='|') {
-			if(next1.get(f)==next2.get(f)) {
-				next2.add(f, state);
+		//if | sets states appropriately
+		if(isNotEmpty() && pattern.get(j)=='|') {
+			if(f > 0) {
+				if(next1.get(f)==next2.get(f)) {
+					next2.set(f, state);
+				}
+				next1.set(f, state);
 			}
-			next1.add(f, state);
 			f=state-1;
 			j++;
 			r=state;
 			state++;
 			t2=term();
-			setState(r, ' ', t1, t2);
+			setState(r, " ", t1, t2);
 			if(next1.get(f)==next2.get(f)) {
-				next2.add(f,state);
+				next2.set(f,state);
 			}
-			next1.add(f, state);
+			next1.set(f, state);
 		}
-		//if ?
-		if(pattern.get(j)=='?') {
-			
-		}
-		//if \
-		if(pattern.get(j)=='\\') {
-			
-		}
-		//if .
-		if(pattern.get(j)=='.') {
-			
+		//if ? sets states appropriately
+		if(isNotEmpty() && pattern.get(j)=='?') {
+			j++;
+			String s = Character.toString(pattern.get(j));
+			setState(state,s,state+1, state+1);
+			next1.set(f, state);
+			next2.set(f, state+1);
+			j++;
+			r=state;
+			state++;
 		}
 		//return initial state
 		return(r);
@@ -135,7 +175,8 @@ public class REcompile {
 	private static int factor() {
 		int r = 0;
 		if(isVocab(pattern.get(j))) {
-			setState(state,pattern.get(j),state+1, state+1);
+			String s = Character.toString(pattern.get(j));
+			setState(state,s,state+1, state+1);
 			j++;
 			r=state;
 			state++;
@@ -150,8 +191,20 @@ public class REcompile {
 				error();
 			}
 		}
-		else {
-			error();
+		//if \ and dealing with edge case '\.'
+		else if(pattern.get(j)=='\\') {
+			if(pattern.get(j+1)=='.') {
+				j+=2;
+				setState(state, "\\.", state+1, state+1);
+			}
+			else {
+				j++;;
+				String s1 = Character.toString(pattern.get(j));
+				setState(state, s1, state+1, state+1);
+				r=state;
+				state++;
+				j++;
+			}
 		}
 		return(r);
 	}
