86baedd816b9a7ea527680c16b677a7c731ce1f5
Nash <nashsmith0510@gmail.com>
2020-05-04 17:44:13 +1200

    Add importFSM method.

--- Pattern Searching/src/REsearch.java
+++ Pattern Searching/src/REsearch.java
@@ -7,12 +7,17 @@ public class REsearch {
     public static List<Integer> next1 = new ArrayList<Integer>();
     public static List<Integer> next2 = new ArrayList<Integer>();
 
+    public static int startState;
     public static int mark;
     public static int pointer;
     public static Deque states;
 
+    String fsmFileName = args[0];
+    String textFileName = args[1];
+
 
     //read FSM data and initialise ArrayLists
+    importFSM(fsmFileName);
 
     //get file inputstream from args
     //read the file line by line into an array of characters
@@ -33,6 +38,34 @@ public class REsearch {
     //if at any point -1 is a currentState then there was a full pattern match
       //output the line
 
+
+  }
+
+  /*
+   *  Takes a text file from REcompile and turns it into the three ArrayLists
+   *  representing the Finite State Machine.
+   */
+  public void importFSM(String fileName){
+    File file = new File(fileName);
+    BufferedReader fsm = new BufferedReader(new FileReader(file));
+    String line;
+    Boolean firstRead = true;
+
+    while((line = fsm.readLine()) != null){
+      if(firstRead){
+        startState = Integer.parseInt(line);
+        firstRead = false;
+      }
+      String[] values = line.split(",");
+      Integer stateNum = Integer.parseInt(values[0]);
+      char c = values[1].charAt(0);
+      Integer n1 = Integer.parseInt(values[2]);
+      Integer n2 = Integer.parseInt(values[3]);
+
+      ch.add(stateNum, c);
+  		next1.add(stateNum, n1);
+  		next2.add(stateNum, n2);
+    }
 
   }
 
