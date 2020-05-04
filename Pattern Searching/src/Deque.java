6da44574c2618d61e1140337587d73951716b724
Nash <nashsmith0510@gmail.com>
2020-05-04 12:57:54 +1200

    Implement Deque

---
+++ Pattern Searching/src/Deque.java
@@ -0,0 +1,40 @@
+class Deque {
+
+  private Stack currentStates;
+  private Queue nextStates;
+
+  //Constructors
+  public Deque(){
+    currentStates = new Stack();
+    nextStates = new Queue();
+
+    //set SCAN
+    currentStates.push("SCAN");
+  }
+
+  //Add a state into the current states
+  public void addCurrentState(int state){
+    currentStates.push(state);
+  }
+
+  //pop off the next state and return it
+  public Object getNextState(){
+    return currentStates.pop();
+  }
+
+  //adds a state to the queue of next states
+  public void addToNextStates(int state){
+    nextStates.add(state);
+  }
+
+  //puts the states in nextStates into the currentStates
+  public void transferStates(){
+    currentStates = new Stack();
+    currentStates.push("SCAN");
+    while(nextStates.peek() != null){
+      currentStates.push(nextStates.remove());
+    }
+  }
+
+
+}
